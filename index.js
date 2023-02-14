975d83c8c4c9c28abdb27d5c4e6f5f28c17554d3
wangmingbao <wangmingbao@guandata.com>
2023-02-14 14:42:14 +0800

    feat: 添加/message/post

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ router.get("/", async (ctx) => {
   ctx.body = homePage;
 });
 
+//  自己加的一个用户发什么消息，就反弹什么消息的消息回复功能
+router.post('/message/post', async ctx => {
+  const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
+
+  ctx.body = {
+    ToUserName: FromUserName,
+    FromUserName: ToUserName,
+    CreateTime: +new Date(),
+    MsgType: 'text',
+    Content: `反弹你发的消息：${Content}`,
+  };
+});
+
 // 更新计数
 router.post("/api/count", async (ctx) => {
   const { request } = ctx;
